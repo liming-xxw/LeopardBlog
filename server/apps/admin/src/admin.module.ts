ea7e9ed58d1ad72721cc0d5436eb66758b25aa86
limingx-xxw <3111345436@qq.com>
2021-12-09 20:00:52 +0800

    解决server环境变量

--- server/apps/admin/src/admin.module.ts
+++ server/apps/admin/src/admin.module.ts
@@ -1,4 +1,3 @@
-import { DbModule } from '@libs/db';
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
@@ -8,20 +7,25 @@ import { MulterModule } from '@nestjs/platform-express';
 import { AdminusersModule } from './adminusers/adminusers.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { TagsModule } from './tags/tags.module';
+import { CommonModule } from '@app/common';
 const MAO = require('multer-aliyun-oss');
 @Module({
   imports: [
-    MulterModule.register({
-      storage: MAO({
-        config: {
-          region: 'oss-cn-hangzhou',
-          accessKeyId: 'LTAI5t9cTejWx6na6quuTFPw',
-          accessKeySecret: 'SiS7YUNxjFWjt264RJZhWDyqoMToyE',
-          bucket: 'whxxw',
-        },
-      }),
+    MulterModule.registerAsync({
+      useFactory() {
+        return {
+          storage: MAO({
+            config: {
+              region: process.env.OSS_REGION,
+              accessKeyId: process.env.OSS_ACCESS_KEY_ID,
+              accessKeySecret: process.env.OSS_ACCRSS_KEY_SECRET,
+              bucket: process.env.OSS__BUCKET,
+            },
+          }),
+        };
+      },
     }),
-    DbModule,
+    CommonModule,
     UsersModule,
     ApiresultModule,
     AdminusersModule,
